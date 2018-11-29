12f6e8e763e903dfdff1a8c863d341997c873ec3
mutianyan <mutianyan@econ1-189-125-dhcp.int.colorado.edu>
2018-11-29 14:53:29 -0700

    convert categorcial attributes to binary

--- dm-project-banking/code/preprocess.py
+++ dm-project-banking/code/preprocess.py
@@ -1,5 +1,6 @@
 import csv
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 """
 fp = open("../bank.csv", "r")
@@ -14,6 +15,7 @@ fp1.close()
 
 """
 
+# bank.csv from May 2008 to November 2010
 
 data_set = pd.read_csv('../data/shortfile',delimiter = ';')
 
@@ -22,10 +24,80 @@ def transforForamt(dataset, diction):
         dataset[i] = diction[dataset[i]]
     return dataset
 
+
+
+
 dic={"yes":1,"no":0}
 data_set['deafult'] = transforForamt(data_set['default'],dic)
 data_set['housing'] = transforForamt(data_set['housing'],dic)
 data_set['loan'] = transforForamt(data_set['loan'],dic)
 data_set['y'] = transforForamt(data_set['y'],dic)
+
+# categorical and continuous variables
+#https://scikit-learn.org/stable/modules/preprocessing.html
+
+
+# change unkown values to different labels
+
+
+data_set.loc[data_set['education'] == 'unknown', 'education'] = 'unknown_education'
+data_set.loc[data_set['contact'] == 'unknown', 'contact'] = 'unknown_contact'
+data_set.loc[data_set['poutcome'] == 'unknown', 'poutcome'] = 'unknown_poutcome'
+
+# pandas get dummies for categorical variables
+
+print(list(data_set))
+
+
+s = data_set.job
+one_hot=pd.get_dummies(s) # return type is dataFrame
+data_set = data_set.join(one_hot)
+data_set = data_set.drop('job',axis=1)
+
+s = data_set.marital
+one_hot=pd.get_dummies(s) # return type is dataFrame
+data_set = data_set.join(one_hot)
+data_set = data_set.drop('marital',axis=1)
+
+s = data_set.education
+one_hot=pd.get_dummies(s) # return type is dataFrame
+data_set = data_set.join(one_hot)
+data_set = data_set.drop('education',axis=1)
+
+s = data_set.contact
+one_hot=pd.get_dummies(s) # return type is dataFrame
+data_set = data_set.join(one_hot)
+data_set = data_set.drop('contact',axis=1)
+
+s = data_set.poutcome
+one_hot=pd.get_dummies(s) # return type is dataFrame
+data_set = data_set.join(one_hot)
+data_set = data_set.drop('poutcome',axis=1)
+
+# ?
+s = data_set.month
+one_hot=pd.get_dummies(s) # return type is dataFrame
+data_set = data_set.join(one_hot)
+data_set = data_set.drop('month',axis=1)
+
+
+
+
+print(list(data_set))
+"""
+#data_set.education = dummies
+enc = OneHotEncoder()
+enc.fit(dummies)
+print(enc.n_values_)
+print(enc.feature_indices_)
+enc.transform([[0, 1, 1, 0]]).toarray()
+"""
+
+
+
+
+
+# split up the data set 
+#https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.TimeSeriesSplit.html
 
 data_set.to_csv("../data/afterTransform.csv", index = False)
