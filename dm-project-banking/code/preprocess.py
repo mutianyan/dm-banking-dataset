2823e1cf1fbf90bafb744cc5c2f548d2dabfd5d1
mutianyan <mutianyan@econ2-255-139-dhcp.int.colorado.edu>
2018-11-30 15:00:16 -0700

    added preprecessed dataset

--- dm-project-banking/code/preprocess.py
+++ dm-project-banking/code/preprocess.py
@@ -1,6 +1,8 @@
 import csv
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
+from sklearn.model_selection import TimeSeriesSplit
 
 """
 fp = open("../bank.csv", "r")
@@ -17,7 +19,7 @@ fp1.close()
 
 # bank.csv from May 2008 to November 2010
 
-data_set = pd.read_csv('../data/shortfile',delimiter = ';')
+data_set = pd.read_csv('../data/bank-full.csv',delimiter = ';')
 
 def transforForamt(dataset, diction):
     for i in range(len(dataset)):
@@ -39,7 +41,7 @@ data_set['y'] = transforForamt(data_set['y'],dic)
 
 # change unkown values to different labels
 
-
+data_set.loc[data_set['job'] == 'unknown', 'job'] = 'unknown_job'
 data_set.loc[data_set['education'] == 'unknown', 'education'] = 'unknown_education'
 data_set.loc[data_set['contact'] == 'unknown', 'contact'] = 'unknown_contact'
 data_set.loc[data_set['poutcome'] == 'unknown', 'poutcome'] = 'unknown_poutcome'
@@ -81,22 +83,32 @@ data_set = data_set.join(one_hot)
 data_set = data_set.drop('month',axis=1)
 
 
+#print(list(data_set))
 
 
-print(list(data_set))
 """
-#data_set.education = dummies
-enc = OneHotEncoder()
-enc.fit(dummies)
-print(enc.n_values_)
-print(enc.feature_indices_)
-enc.transform([[0, 1, 1, 0]]).toarray()
+# dynamic split 
+y_df = data_set.y
+data_set.drop('y', axis= 1)
+X_df = data_set
+
+y = np.array(y_df)
+X = np.array(X_df)
+
+print(y)
+print(X)
+
+#X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
+#y = np.array([1, 2, 3, 4, 5, 6])
+tscv = TimeSeriesSplit(n_splits=2, max_train_size = 4)
+print(tscv)  
+for train_index, test_index in tscv.split(X):
+	# use model to predict 
+	print("TRAIN:", train_index, "TEST:", test_index)
+	X_train, X_test = X[train_index], X[test_index]
+	y_train, y_test = y[train_index], y[test_index]
+
 """
-
-
-
-
-
 # split up the data set 
 #https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.TimeSeriesSplit.html
 
