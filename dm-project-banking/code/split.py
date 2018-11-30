ea88e8f810ad9f3058bcc653dd7b05e081ca073c
Yuchen <yuchenzhang.study@gmail.com>
2018-11-30 16:44:46 -0700

    Split

--- dm-project-banking/code/split.py
+++ dm-project-banking/code/split.py
@@ -1,10 +1,13 @@
 import numpy as np
-from sklearn.model_selection import TimeSeriesSplit
-X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4],[1, 2], [3, 4], [1, 2], [3, 4], [1, 2], [3, 4]])
-y = np.array([1, 2, 3, 4, 5, 6,1, 2, 3, 4, 5, 6])
-tscv = TimeSeriesSplit(n_splits=3, max_train_size = 4)
-print(tscv)  
-for train_index, test_index in tscv.split(X):
-	print("TRAIN:", train_index, "TEST:", test_index)
-	X_train, X_test = X[train_index], X[test_index]
-	y_train, y_test = y[train_index], y[test_index]
+
+
+def rolling_window_split(data,W,K):
+	train_idx=[]
+	test_idx=[]
+	loop=int((data.shape[0]-W)/K)
+	for i in range(loop):
+		train_idx.append(range(i*K,i*K+W))
+		test_idx.append(range(W+i*K,W+(i+1)*K))
+	return train_idx,test_idx
+
+
